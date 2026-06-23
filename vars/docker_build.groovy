60be0000cc24f36ebc87db28f21bbe6fbbf29896
Tejash-TS <151548044+Tejash-TS@users.noreply.github.com>
2026-06-23 19:18:57 +0530

    Fix parameter declaration in docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String Projectname String imageTag ,string DockerHubUser){
+def call(String Projectname ,String imageTag ,String DockerHubUser){
 sh "docker build -t ${DockerHubUser}/${Projectname}:${ImageTag} ."
 }
